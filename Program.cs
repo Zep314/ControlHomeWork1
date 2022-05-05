b0b35648f900a71fcbbcc4bcbce8ca337373ceef
Dmitry Mordanov <mordanov314@gmail.com>
2022-05-05 14:37:48 +0300

    Program.cs draft completed

--- Program.cs
+++ Program.cs
@@ -12,4 +12,48 @@
 
 using System;
 
-Console.WriteLine("Hello, World!");
+
+// void MyPrintArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
+
+static void MyPrintArray(string[] localArray)
+{
+    Console.Write("[");
+    for (int i =0; i < localArray.Length; i++)
+    {
+        Console.Write($"\"{localArray[i]}\"");
+        if (i != localArray.Length - 1) Console.Write(", ");
+    }
+    Console.WriteLine("]");
+}
+
+Console.Clear();
+Console.WriteLine("Итоговая проверочная работа.");
+Console.Write("Введите количество строк исходного массива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string [] iArray = new string [n];
+int m = 0;
+for (int i=0; i<iArray.Length; i++)
+{
+    Console.Write($"Введите {i+1}-ю строку: ");
+    iArray[i] = Console.ReadLine();
+    if (iArray[i].Length<4)
+    {
+        m++;
+    }
+}
+Console.WriteLine("Исходный массив: ");
+MyPrintArray(iArray);
+
+string [] rArray = new string [m];
+int k = 0;
+for(int i=0; i<iArray.Length; i++)
+{
+    if (iArray[i].Length<4)
+    {
+        rArray[k] = iArray[i];
+        k++;
+    }
+}
+
+Console.WriteLine("Результирующий массив: ");
+MyPrintArray(rArray);
