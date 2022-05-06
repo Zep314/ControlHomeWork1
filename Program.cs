45c1faa709673c5fca5832e2a4380f1e6d254863
Dmitry Mordanov <mordanov314@gmail.com>
2022-05-06 12:38:17 +0300

    prefinalized commit

--- Program.cs
+++ Program.cs
@@ -9,19 +9,15 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
-
 using System;
 
-
-// void MyPrintArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
-
-static void MyPrintArray(string[] localArray)
+void MyPrintArray(string[] localArray)  // Красивый вывод на печать массива строк
 {
     Console.Write("[");
     for (int i =0; i < localArray.Length; i++)
     {
-        Console.Write($"\"{localArray[i]}\"");
-        if (i != localArray.Length - 1) Console.Write(", ");
+        Console.Write($"\"{localArray[i]}\""); // Печатаем текущую строку массива в кавычках
+        if (i != localArray.Length - 1) Console.Write(", ");  // после последнего элемента не печатаем ", "
     }
     Console.WriteLine("]");
 }
@@ -30,9 +26,12 @@ Console.Clear();
 Console.WriteLine("Итоговая проверочная работа.");
 Console.Write("Введите количество строк исходного массива n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-string [] iArray = new string [n];
-int m = 0;
-for (int i=0; i<iArray.Length; i++)
+
+string [] iArray = new string [n];  // Исходный массив
+
+int m = 0;  // Тут храним количество элементов исходного массива, которые менее 4-х символов
+
+for (int i=0; i<iArray.Length; i++)  // Ввод строк и подсчет элементов, менее 4-х символов
 {
     Console.Write($"Введите {i+1}-ю строку: ");
     iArray[i] = Console.ReadLine();
@@ -41,11 +40,14 @@ for (int i=0; i<iArray.Length; i++)
         m++;
     }
 }
+
 Console.WriteLine("Исходный массив: ");
 MyPrintArray(iArray);
 
 string [] rArray = new string [m];
-int k = 0;
+
+int k = 0; // Тут храним индекс результирующего массива
+
 for(int i=0; i<iArray.Length; i++)
 {
     if (iArray[i].Length<4)
